a5b25645846105acc8da05cb86971b5a544df7a9
caioreis123 <reis.caio@gmail.com>
2019-04-23 12:49:47 -0300

    details placeholder

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -1,11 +1,49 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { Grid, withStyles } from '@material-ui/core'
+import testImage from '../product-1.png'
 
-export default class Details extends Component {
-  render() {
+const styles = () => ({
+    root: {
+        width: '70%',
+        margin: 'auto',
+    },
+    img: {
+        maxWidth: '400px',
+        height: 'auto',
+        // width: 'auto',
+        // maxHeight: '400px',
+
+    },
+    txt: {
+        textAlign: "justify",
+    }
+});
+
+const Details = (props) => {
+    const { classes } = props;
+
     return (
-      <div>
-        <h3>hello from details</h3>
-      </div>
+        <Grid
+            container
+            direction="row"
+            align="center"
+            alignItems="center"
+            justify='center'
+            className={classes.root}
+        >
+            <Grid item xs={12}>
+                <h1>Samsung X</h1>
+            </Grid>
+            <Grid className={classes.img} item xs={6} >
+                <img src={testImage} alt='product' />
+            </Grid>
+            <Grid className={classes.txt} item xs={6} justify='flex-start' >
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum, cum! Ratione deleniti incidunt, quasi nisi aperiam dolores mollitia obcaecati corporis.</p>
+            </Grid>
+        </Grid>
     )
-  }
 }
+
+
+export default withStyles(styles)(Details);
+
