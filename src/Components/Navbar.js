57536dcca0b74b0866a96751c510c0f6de0212c7
caioreis123 <reis.caio@gmail.com>
2019-05-03 14:12:30 -0300

    badge

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,31 +6,53 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import { Link } from 'react-router-dom'
+import Badge from '@material-ui/core/Badge';
+import {MyConsumer} from './../Context'
 
-const styles = {
+const styles = theme => ({
     cart: {
         marginLeft: 'auto',
     },
-};
+    badge: {
+        top: '50%',
+        right: -3,
+        border: `2px solid ${theme.palette.grey[200]}`
+    },
+});
 
 function Navbar(props) {
     const { classes } = props;
     return (
-        <div>
-            <AppBar position="static">
-                <Toolbar>
-                    <IconButton component={Link} to = "/" color="inherit" aria-label="Store">
-                        <i className="material-icons">store</i>
-                        <Typography variant="h6" color="inherit">
-                            Store
-                        </Typography>
-                    </IconButton>
-                    <IconButton component={Link} to="/cart" className={classes.cart} color="inherit" aria-label="Cart">  
-                        <i className="material-icons">local_grocery_store</i>
-                    </IconButton>
-                </Toolbar>
-            </AppBar>
-        </div>
+        <MyConsumer>
+            {value => {
+                return (    
+                    <div>
+                        <AppBar position="static">
+                            <Toolbar>
+                                <IconButton component={Link} to = "/" color="inherit" aria-label="Store">
+                                    <i className="material-icons">store</i>
+                                    <Typography variant="h6" color="inherit">
+                                        Store
+                                    </Typography>
+                                </IconButton>
+
+                                <IconButton 
+                                component={Link} 
+                                to="/cart" 
+                                className={classes.cart} 
+                                color="inherit" 
+                                aria-label="Cart">
+                                    <Badge badgeContent={value.cart.length} color="primary" classes={{ badge: classes.badge }}>  
+                                        <i className="material-icons">local_grocery_store</i>
+                                    </Badge>
+                                </IconButton>
+
+                            </Toolbar>
+                        </AppBar>
+                    </div>
+                )
+            }}
+    </MyConsumer>
     );
 }
 
