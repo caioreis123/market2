b2c960030f89099098eecbb65804456829647549
caioreis123 <reis.caio@gmail.com>
2019-07-12 14:32:44 -0300

    details query is working!!

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -1,122 +1,117 @@
 /* this page is shown when the user clicks the img of a product */
-import React from "react"
-import { Grid, withStyles, Button, Icon } from "@material-ui/core"
-import { MyConsumer } from "./../Context"
-import { Link } from "react-router-dom"
+import React from 'react'
+import { Grid, withStyles, Button, Icon } from '@material-ui/core'
+import { MyConsumer } from './../Context'
+import { Link } from 'react-router-dom'
 
 //relay imports:
-import graphql from "babel-plugin-relay/macro"
-import { createFragmentContainer } from "react-relay"
-import { QueryRenderer } from "react-relay"
-import environment from "../environment"
+import graphql from 'babel-plugin-relay/macro'
+import { createFragmentContainer } from 'react-relay'
+import { QueryRenderer } from 'react-relay'
+import environment from '../environment'
 
-const styles = theme => ({
-  root: {
-    width: "70%",
-    margin: "auto"
-  },
-  img: {
-    maxWidth: "400px",
-    height: "auto"
-  },
-  txt: {
-    textAlign: "justify"
-  },
-  btn: {
-    margin: theme.spacing.unit
-  },
-  made: {
-    textTransform: "uppercase"
-  }
+const styles = (theme) => ({
+	root: {
+		width: '70%',
+		margin: 'auto'
+	},
+	img: {
+		maxWidth: '400px',
+		height: 'auto'
+	},
+	txt: {
+		textAlign: 'justify'
+	},
+	btn: {
+		margin: theme.spacing.unit
+	},
+	made: {
+		textTransform: 'uppercase'
+	}
 })
 
 const DetailsQuery = graphql`
-  query DetailsQuery($ID: ProductWhereUniqueInput!) {
-    product(where: $ID) {
-      ...Details_product
-    }
-  }
+	query DetailsQuery($ID: ProductWhereUniqueInput!) {
+		product(where: $ID) {
+			id
+			title
+			img
+			price
+			subTotal
+			count
+			company
+			info
+		}
+	}
 `
 
-const SimpleDetails = props => {
-  const { classes, match } = props
-  let id = match.params.id
-  return (
-    <QueryRenderer
-      environment={environment}
-      query={DetailsQuery}
-      variables={{ ID: { id } }}
-      render={({ error, props }) => {
-        if (error) {
-          return <div>{error.message}</div>
-        } else if (!props) {
-          return <div>Loading...</div>
-        }
-        return (
-          <MyConsumer>
-            {value => {
-              return (
-                <Grid
-                  container
-                  direction="row"
-                  align="center"
-                  alignItems="center"
-                  justify="center"
-                  className={classes.root}
-                >
-                  <Grid item xs={12}>
-                    <h1>{props.product.title}</h1>
-                  </Grid>
-                  <Grid item xs={6} className={classes.img}>
-                    <img src={props.product.img} alt="product" />
-                  </Grid>
-                  <Grid
-                    item
-                    xs={6}
-                    className={classes.txt}
-                    justify="flex-start"
-                  >
-                    <h2 className={classes.made}>
-                      MADE BY: {props.product.company}
-                    </h2>
-                    <h3>Price: ${props.product.price}</h3>
-                    <p>
-                      {" "}
-                      <strong>Some Info About the Product: </strong>
-                    </p>
-                    <p>{props.product.info}</p>
-                    <Button
-                      component={Link}
-                      to="/"
-                      variant="contained"
-                      className={classes.btn}
-                    >
-                      <Icon>keyboard_backspace</Icon>
-                      Back to Store
-                    </Button>
-                    <Button
-                      onClick={() => {
-                        value.addToCart(props.product.id)
-                      }}
-                      variant="contained"
-                      color="primary"
-                      className={classes.btn}
-                    >
-                      {"Add to Cart"}
-                      <Icon>add_shopping_cart</Icon>
-                    </Button>
-                  </Grid>
-                </Grid>
-              )
-            }}
-          </MyConsumer>
-        )
-      }}
-    />
-  )
+const Details = (props) => {
+	const { classes, match } = props
+	let id = match.params.id
+	return (
+		<QueryRenderer
+			environment={environment}
+			query={DetailsQuery}
+			variables={{ ID: { id } }}
+			render={({ error, props }) => {
+				if (error) {
+					return <div>{error.message}</div>
+				} else if (!props) {
+					return <div>Loading...</div>
+				}
+				return (
+					<MyConsumer>
+						{(value) => {
+							return (
+								<Grid
+									container
+									direction="row"
+									align="center"
+									alignItems="center"
+									justify="center"
+									className={classes.root}
+								>
+									<Grid item xs={12}>
+										<h1>{props.product.title}</h1>
+									</Grid>
+									<Grid item xs={6} className={classes.img}>
+										<img src={props.product.img} alt="product" />
+									</Grid>
+									<Grid item xs={6} className={classes.txt} justify="flex-start">
+										<h2 className={classes.made}>MADE BY: {props.product.company}</h2>
+										<h3>Price: ${props.product.price}</h3>
+										<p>
+											{' '}
+											<strong>Some Info About the Product: </strong>
+										</p>
+										<p>{props.product.info}</p>
+										<Button component={Link} to="/" variant="contained" className={classes.btn}>
+											<Icon>keyboard_backspace</Icon>
+											Back to Store
+										</Button>
+										<Button
+											onClick={() => {
+												value.addToCart(props.product.id)
+											}}
+											variant="contained"
+											color="primary"
+											className={classes.btn}
+										>
+											{'Add to Cart'}
+											<Icon>add_shopping_cart</Icon>
+										</Button>
+									</Grid>
+								</Grid>
+							)
+						}}
+					</MyConsumer>
+				)
+			}}
+		/>
+	)
 }
 
-const Details = createFragmentContainer(SimpleDetails, {
+/* const Details = createFragmentContainer(SimpleDetails, {
   product: graphql`
     fragment Details_product on Product {
       id
@@ -129,7 +124,7 @@ const Details = createFragmentContainer(SimpleDetails, {
       info
     }
   `
-})
+}) */
 
 export default withStyles(styles)(Details)
 
--- src/Components/__generated__/DetailsQuery.graphql.js
+++ src/Components/__generated__/DetailsQuery.graphql.js
@@ -1,6 +1,6 @@
 /**
  * @flow
- * @relayHash 9cb05aa5c815478f6b45fcbc8053ebf8
+ * @relayHash 62da286fab151aaa0430bb6b8f6328c9
  */
 
 /* eslint-disable */
@@ -9,7 +9,6 @@
 
 /*::
 import type { ConcreteRequest } from 'relay-runtime';
-type Details_product$ref = any;
 export type ProductWhereUniqueInput = {|
   id?: ?string
 |};
@@ -18,7 +17,14 @@ export type DetailsQueryVariables = {|
 |};
 export type DetailsQueryResponse = {|
   +product: ?{|
-    +$fragmentRefs: Details_product$ref
+    +id: string,
+    +title: string,
+    +img: string,
+    +price: number,
+    +subTotal: number,
+    +count: number,
+    +company: string,
+    +info: string,
   |}
 |};
 export type DetailsQuery = {|
@@ -33,20 +39,15 @@ query DetailsQuery(
   $ID: ProductWhereUniqueInput!
 ) {
   product(where: $ID) {
-    ...Details_product
     id
+    title
+    img
+    price
+    subTotal
+    count
+    company
+    info
   }
-}
-
-fragment Details_product on Product {
-  id
-  title
-  img
-  price
-  subTotal
-  count
-  company
-  info
 }
 */
 
@@ -61,9 +62,77 @@ var v0 = [
 ],
 v1 = [
   {
-    "kind": "Variable",
-    "name": "where",
-    "variableName": "ID"
+    "kind": "LinkedField",
+    "alias": null,
+    "name": "product",
+    "storageKey": null,
+    "args": [
+      {
+        "kind": "Variable",
+        "name": "where",
+        "variableName": "ID"
+      }
+    ],
+    "concreteType": "Product",
+    "plural": false,
+    "selections": [
+      {
+        "kind": "ScalarField",
+        "alias": null,
+        "name": "id",
+        "args": null,
+        "storageKey": null
+      },
+      {
+        "kind": "ScalarField",
+        "alias": null,
+        "name": "title",
+        "args": null,
+        "storageKey": null
+      },
+      {
+        "kind": "ScalarField",
+        "alias": null,
+        "name": "img",
+        "args": null,
+        "storageKey": null
+      },
+      {
+        "kind": "ScalarField",
+        "alias": null,
+        "name": "price",
+        "args": null,
+        "storageKey": null
+      },
+      {
+        "kind": "ScalarField",
+        "alias": null,
+        "name": "subTotal",
+        "args": null,
+        "storageKey": null
+      },
+      {
+        "kind": "ScalarField",
+        "alias": null,
+        "name": "count",
+        "args": null,
+        "storageKey": null
+      },
+      {
+        "kind": "ScalarField",
+        "alias": null,
+        "name": "company",
+        "args": null,
+        "storageKey": null
+      },
+      {
+        "kind": "ScalarField",
+        "alias": null,
+        "name": "info",
+        "args": null,
+        "storageKey": null
+      }
+    ]
   }
 ];
 return {
@@ -74,108 +143,23 @@ return {
     "type": "Query",
     "metadata": null,
     "argumentDefinitions": (v0/*: any*/),
-    "selections": [
-      {
-        "kind": "LinkedField",
-        "alias": null,
-        "name": "product",
-        "storageKey": null,
-        "args": (v1/*: any*/),
-        "concreteType": "Product",
-        "plural": false,
-        "selections": [
-          {
-            "kind": "FragmentSpread",
-            "name": "Details_product",
-            "args": null
-          }
-        ]
-      }
-    ]
+    "selections": (v1/*: any*/)
   },
   "operation": {
     "kind": "Operation",
     "name": "DetailsQuery",
     "argumentDefinitions": (v0/*: any*/),
-    "selections": [
-      {
-        "kind": "LinkedField",
-        "alias": null,
-        "name": "product",
-        "storageKey": null,
-        "args": (v1/*: any*/),
-        "concreteType": "Product",
-        "plural": false,
-        "selections": [
-          {
-            "kind": "ScalarField",
-            "alias": null,
-            "name": "id",
-            "args": null,
-            "storageKey": null
-          },
-          {
-            "kind": "ScalarField",
-            "alias": null,
-            "name": "title",
-            "args": null,
-            "storageKey": null
-          },
-          {
-            "kind": "ScalarField",
-            "alias": null,
-            "name": "img",
-            "args": null,
-            "storageKey": null
-          },
-          {
-            "kind": "ScalarField",
-            "alias": null,
-            "name": "price",
-            "args": null,
-            "storageKey": null
-          },
-          {
-            "kind": "ScalarField",
-            "alias": null,
-            "name": "subTotal",
-            "args": null,
-            "storageKey": null
-          },
-          {
-            "kind": "ScalarField",
-            "alias": null,
-            "name": "count",
-            "args": null,
-            "storageKey": null
-          },
-          {
-            "kind": "ScalarField",
-            "alias": null,
-            "name": "company",
-            "args": null,
-            "storageKey": null
-          },
-          {
-            "kind": "ScalarField",
-            "alias": null,
-            "name": "info",
-            "args": null,
-            "storageKey": null
-          }
-        ]
-      }
-    ]
+    "selections": (v1/*: any*/)
   },
   "params": {
     "operationKind": "query",
     "name": "DetailsQuery",
     "id": null,
-    "text": "query DetailsQuery(\n  $ID: ProductWhereUniqueInput!\n) {\n  product(where: $ID) {\n    ...Details_product\n    id\n  }\n}\n\nfragment Details_product on Product {\n  id\n  title\n  img\n  price\n  subTotal\n  count\n  company\n  info\n}\n",
+    "text": "query DetailsQuery(\n  $ID: ProductWhereUniqueInput!\n) {\n  product(where: $ID) {\n    id\n    title\n    img\n    price\n    subTotal\n    count\n    company\n    info\n  }\n}\n",
     "metadata": {}
   }
 };
 })();
 // prettier-ignore
-(node/*: any*/).hash = '9ea03524fcce42c1883c8641c8d9f46a';
+(node/*: any*/).hash = '2535caef710376c2e1da4c9c81baa55a';
 module.exports = node;
