8cec1800740f9500705c4888172ab243079899c8
caioreis123 <reis.caio@gmail.com>
2019-07-18 16:27:10 -0300

    added buy button without functionality yet

--- src/Components/Cart/CartTotal.js
+++ src/Components/Cart/CartTotal.js
@@ -1,28 +1,51 @@
-import React from 'react'
-import { withStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import { Typography } from '@material-ui/core';
+import React from "react"
+import { withStyles } from "@material-ui/core/styles"
+import Button from "@material-ui/core/Button"
+import { Typography, Grid } from "@material-ui/core"
 
-const styles = theme => ({
-    button: {
-        // margin: theme.spacing.unit,
-        display: "flex",
-        margin: "auto",
-    }
-});
+const styles = (theme) => ({
+	root: {
+		flexGrow: 1,
+		paddingRight: "5rem",
+		//justifyContent: "spaceBetween",
+	},
+	total: {
+		//float: "center",
+	},
+	buttonClear: {
+		//display: "inlineFlex",
+		//marginLeft: "auto",
+		//float: "left",
+	},
+	buttonBuy: {
+		//display: "inlineBlock",
+		padding: "6px 18px",
+		//float: "right",
+	},
+})
 
 function CartTotal(props) {
-    const {classes} = props
-  return (
-      <div>
-        <Typography align="center" variant="h5" >
-            Total: ${props.value.cartTotalValue}
-        </Typography>
-          <Button onClick={() => { props.value.deepCopyProducts()}} variant="contained" color="secondary" className={classes.button}>
-              Clear Cart
-        </Button>
-    </div>
-  )
+	const { classes } = props
+	return (
+		<Grid container direction="column" justify="space-evenly" alignItems="flex-end" className={classes.root}>
+			<Typography className={classes.total} align="center" variant="h5">
+				Total: ${props.value.cartTotalValue}
+			</Typography>
+			<Button
+				className={classes.buttonClear}
+				onClick={() => {
+					props.value.deepCopyProducts()
+				}}
+				variant="contained"
+				color="secondary"
+			>
+				Clear Cart
+			</Button>
+			<Button className={classes.buttonBuy} color="primary" variant="contained">
+				Buy it now
+			</Button>
+		</Grid>
+	)
 }
 
-export default withStyles(styles)(CartTotal);
+export default withStyles(styles)(CartTotal)
