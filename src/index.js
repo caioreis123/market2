c0b13e8d7e5cddd2dbdc26f8740eb9960d3c4046
caioreis123 <reis.caio@gmail.com>
2019-08-26 16:55:55 -0300

    removed unused serviceWorker

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
 import App from "./App"
-import * as serviceWorker from "./serviceWorker"
 import { BrowserRouter as Router } from "react-router-dom"
 import ContextProvider from "./Context"
 
@@ -55,8 +54,3 @@ ReactDOM.render(
 	/>,
 	document.getElementById("root"),
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
